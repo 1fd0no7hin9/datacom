cf2b2278ea066daaa784fa378464fd07dd9cba84
nirachit <finality_earth@hotmail.co.th>
2018-04-28 16:36:25 +0700

    Add files via upload
    complete file lab_09 tx not rx

---
+++ lab09/lab09.ino
@@ -0,0 +1,60 @@
+#include <Wire.h>
+#include <Adafruit_MCP4725.h>
+Adafruit_MCP4725 dac;
+
+#define defaultFreq 1700 //DAC speed (Hz)
+/*freq0 : frequency of carrier sine wave (Hz)*/
+#define freq0 500
+/*A[0]-A[3] : ASK Amplitude (0,5] (V)*/
+const float A[4] = {0.25,0.5,0.75,1};
+/*S_DAC : Amplitude (12bit) of sine wave at 0,90,180,270*/
+const uint16_t S_DAC[4] = {0, 2047, 4095, 2047};
+int delay0;
+char inData[20]; // Allocate some space for the string
+
+void setup( ) {
+/* set buadrate serial is 115200 */
+  Serial.begin(115200);  
+  dac.begin(0x62); // set to default
+  delay0 = (1000000/freq0 - 1000000/defaultFreq)/4;
+  // delay for sampling period of sine
+  // (Tsine – delayfrom DAC processing speed)
+  Serial.flush(); // for clear buffer serial
+}
+
+void loop( ) {
+  
+  if(Serial.available()>0){
+    
+    /*use a cycle loop receive inData : message input */
+    for (int i=0; i==0 || inData[i-1]!=0xFFFFFFFF; i++){
+      inData[i] = Serial.read(); // Read a character
+    }
+
+    /*use a cycle loop i for send data 8 bits*/
+    for (int i=0; inData[i]!=0xFFFFFFFF; i++ ){
+    /*
+    use a cycle loop k for 1 ASK signal element (2 bit)
+    - map inData[i] to tmp (2 bit)
+    - from LSB to MSB
+    */
+      for (int k=7; k>=0; k-=2){
+        int tmp = inData[i] & B11000000 ; // 00, 01, 10, 11
+        /*use a cycle loop sl to send 5 cycle/baud*/
+        for(int s2=0; s2<5; s2++ ){
+        /*use a cycle loop s to send 4 sample/cycle*/
+          for(int sl=0; sl<4; sl++ ){
+            /*
+            Use the selected amplitude above to modify
+            sine amplitude
+            */
+            dac.setVoltage(S_DAC[sl]*A[tmp],false);
+            delayMicroseconds(delay0); // sampling delay
+          }
+        }
+      inData[i] <<= 2;
+      }
+    }
+    dac.setVoltage(0,false); // for don't send
+  }
+}
---
+++ lab09/lab09_RX/lab09_RX.ino
@@ -0,0 +1,82 @@
+#include <Wire.h>
+#include <Adafruit_MCP4725.h>
+Adafruit_MCP4725 dac;
+
+#define defaultFreq 1700 //DAC speed (Hz)
+/*freq0 : frequency of carrier sine wave (Hz)*/
+#define freq0 500
+/*A[0]-A[3] : ASK Amplitude (0,5] (V)*/
+const float A[4] = {0.25,0.5,0.75,1};
+int delay0;
+/* amin/amax : Amplitude in digital 10bit */
+
+#define a0min 100 /* a0min <= a0 <= a0max */
+#define a0max 300
+#define a1min 400 /* a1min <= a1 <= a1max */
+#define a1max 600
+#define a2min 700/* a2min <= a2 <= a2max */
+#define a2max 800
+#define a3min 900 /* a3min <= a3 <= a3max */
+#define a3max 1100
+/* amplitude difference for detecting rising or falling
+signal */
+#define r_slope 50
+int sum = 0;
+int maxx = 0;
+int prev = 0;
+int check = false;
+int output = -1;
+int count = 0;
+
+void setup() {
+/* set serial baudrate the same as in TX */
+  Serial.begin(115200);
+ 
+}
+
+void loop() {
+  int tmp = analogRead(A0); // read signal from analog pin
+  
+ 
+  if( tmp-prev>r_slope &&check ==false){
+    maxx = 0;
+    check = true; // change check status is true
+//     Serial.println("read temp:");
+//    Serial.println(tmp);
+//    Serial.println("check");
+//    Serial.println(check);
+  
+    
+  }
+  if(tmp>maxx){ // update max value
+    maxx=tmp;
+  }
+  if(maxx-tmp > r_slope){ // check for falling signal
+    if(check == true){
+//      Serial.print("Max");
+//    Serial.println(maxx);
+      if(a0min<maxx && maxx<a0max){
+        Serial.print("0 0 ");
+        count++;
+      }
+      else if(a1min<maxx && maxx<a1max){
+        Serial.print("0 1 ");
+        count++;
+      }
+      else if(a2min<maxx && maxx<a2max){
+        Serial.print("1 0 ");
+        count++;
+      }
+      else if(a3min<maxx && maxx<a3max){
+        Serial.print("1 1 ");
+        count++;
+      }
+      if(count == 5){
+        Serial.println();
+        count = 0;
+      }
+    }
+  check = false; // change check status is false
+  }
+  prev = tmp; // assign temp value to previous
+}
